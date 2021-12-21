48d3352a8d066adbcd6489ae2c99b94ba50499e5
hilmifawwaz <95919284+hilmifawwaz@users.noreply.github.com>
2021-12-21 22:19:02 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-dark">
   <div class="container-fluid">
-    <span class="navbar-brand mb-0 h1">Navbar</span>
+    <span class="navbar-brand mb-0 h1">Rekayasa Perangkat Lunak</span>
   </div>
 </nav>
